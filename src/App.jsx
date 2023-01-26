5ea11d8601568816bc8d7ec350e39ebb7b69118b
Gerard Morte <gerardmorte@gmail.com>
2023-01-26 10:46:09 +0100

    Create React + Tailwind project with Vite

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+const DEFAULT_PROMPT =
+  "Generate a list of 10 names for a web domain using o merge the next words/concepts: domain, idea.";
+
+function App() {
+  return (
+    <div className="text-3xl text-white text-center flex justify-center items-center h-screen">
+      <h1>domidea 💡</h1>
+    </div>
+  );
+}
+
+export default App;
