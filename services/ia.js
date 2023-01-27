3791e84f376d408479d3605b961306c6464c6a67
Gerard Morte <gerardmorte@gmail.com>
2023-01-27 18:44:01 +0100

    Add inputs and button to get words to generate domain

--- services/ia.js
+++ services/ia.js
@@ -10,33 +10,39 @@ Generate new example
 */
 
 export async function domainNameGenerator() {
-  const data = {
-    model: "command-xlarge-20221108",
-    prompt: `Generate a short creative name for a web domain using the following words: domain, idea
-    Example 1: www.domainidea.com
-    Example 2: www.ideadomain.org
-    Generate new example`,
-    max_tokens: 25,
-    temperature: 1,
-    k: 0,
-    p: 0.75,
-    frequency_penalty: 1,
-    presence_penalty: 0,
-    stop_sequences: ["--"],
-    return_likelihoods: "NONE",
-  };
-  const response = await fetch(COHERE_API_GENERATE_URL, {
-    method: "POST",
-    headers: {
-      Authorization: `BEARER ${COHERE_API_KEY}`,
-      "Content-Type": "application/json",
-      "Cohere-Version": "2022-12-06",
-    },
-    body: JSON.stringify(data),
-  }).then((res) => res.json());
+  try {
+    const data = {
+      model: "command-xlarge-20221108",
+      prompt: `Generate a short creative name for a web domain using the following words: domain, idea
+      Example 1: www.domainidea.com
+      Example 2: www.ideadomain.org
+      Generate new example`,
+      max_tokens: 25,
+      temperature: 1.50,
+      k: 0,
+      p: 0.75,
+      frequency_penalty: 1,
+      presence_penalty: 1,
+      // stop_sequences: ["--"],
+      return_likelihoods: "NONE",
+      truncate: "END",
+    };
+    const response = await fetch(COHERE_API_GENERATE_URL, {
+      method: "POST",
+      headers: {
+        accept: "application/json",
+        "Cohere-Version": "2022-12-06",
+        "Content-Type": "application/json",
+        authorization: `Bearer ${COHERE_API_KEY}`,
+      },
+      body: JSON.stringify(data),
+    }).then((res) => res.json());
 
-  console.log(response);
+    console.log(response);
 
-  const { text } = response.generations[0];
-  return text;
+    const { text } = response.generations[0];
+    return text;
+  } catch (error) {
+    console.error(error);
+  }
 }
--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,62 @@
 import "./App.css";
 import { domainNameGenerator } from "../services/ia";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 
-function App() {
+export default function App() {
+  const [generatedName, setGeneratedName] = useState(null);
+  const [input1, setInput1] = useState("");
+  const [input2, setInput2] = useState("");
+  const [input3, setInput3] = useState("");
+  const [inputsArray, setInputsArray] = useState([]);
+
+  const handleInput1Change = (e) => setInput1(e.target.value);
+  const handleInput2Change = (e) => setInput2(e.target.value);
+  const handleInput3Change = (e) => setInput3(e.target.value);
+
+  const handleButtonClick = () => {
+    setInputsArray([input1, input2, input3]);
+  };
+
   useEffect(() => {
-    const getData = async () => {
-      let promise = domainNameGenerator();
-      const value = await promise;
-      console.log(value);
-    };
-    getData();
+    console.log(inputsArray);
+  }, [inputsArray]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const data = await domainNameGenerator();
+      setGeneratedName(data);
+    }
+    fetchData();
   }, []);
 
   return (
-    <div className="text-3xl text-white text-center flex justify-center items-center h-screen">
+    <div className="text-3xl text-white text-center flex flex-col justify-center items-center h-screen">
       <h1>domidea 💡</h1>
+      <div>{generatedName ? <p>{generatedName}</p> : <p>Loading...</p>}</div>
+      <input
+        className="mt-2 mb-2 text-black"
+        type="text"
+        onChange={handleInput1Change}
+        value={input1}
+      />
+      <input
+        className="mb-2 text-black"
+        type="text"
+        onChange={handleInput2Change}
+        value={input2}
+      />
+      <input
+        className="mb-2 text-black"
+        type="text"
+        onChange={handleInput3Change}
+        value={input3}
+      />
+      <button
+        className="border-2 border-white p-2 mt-2"
+        onClick={handleButtonClick}
+      >
+        Generate name!
+      </button>
     </div>
   );
 }
-
-export default App;
