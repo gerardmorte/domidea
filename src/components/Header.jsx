65baad6ac28b3e7b01c61606beeb603493b304ca
Gerard Morte <gerardmorte@gmail.com>
2023-01-30 18:31:58 +0100

    style(Header): Modify styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ export default function Header() {
       <header className="flex justify-between gap-20 w-10/12 mx-auto py-3 text-white text-3xl">
         <div className="flex flex-row gap-2">
           <FcIdea />
-          <h1 className=" text-purple-500 font-bold">domidea</h1>
+          <h1 className=" text-purple-600 font-bold">domidea</h1>
         </div>
         <a
           href="https://github.com/gerardmorte/domidea"
